5336ac42540510094e9e49ce2fcf3774e58cb3fc
hyejinkim99@unist.ac.kr <happyhddey>
2021-03-26 16:02:18 +0900

    feat: Make project2.2 files and Add 'DuckTravelModel' corresponding to 'model' of MVC pattern.

---
+++ project2/index_0327.js
@@ -0,0 +1,53 @@
+/* DuckTravelModel */
+
+const DuckTravelModel = function(size = 7){
+    this.mapSize = size;
+    this.duckMap = [];
+    this.duckLocation = {x:0, y:0};
+    this.curLocation = {x:0, y:0};
+    this.currentTime = 0;
+
+    this.initDuckLocation = function(){
+
+    }
+
+    this.initCurLocation = function(){
+
+    }
+
+    this.setDuckMap = function(){
+
+    }
+
+    this.setCurLocation = function(){
+
+    }
+
+    this.getDuckMap = function(){
+        
+    }
+
+    this.getDuckLocation = function(){
+
+    }
+    
+    this.getCurLocation = function(){
+
+    }
+
+    this.computeNewLocation = function(){
+
+    }
+
+    this.checkRange = function(){
+
+    }
+
+    this.moveDuck = function(){
+
+    }
+
+    this.isSameLocation = function(){
+
+    }
+}
