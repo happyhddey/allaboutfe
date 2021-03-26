bd3bc08e8da874316a9057a14b50239654a861d4
hyejinkim99@unist.ac.kr <happyhddey>
2021-03-26 16:05:59 +0900

    feat: Add a feature to 'DuckTravelModel'

--- project2/index_0327.js
+++ project2/index_0327.js
@@ -1,4 +1,4 @@
-/* DuckTravelModel */
+/* Model: DuckTravelModel */
 
 const DuckTravelModel = function(size = 7){
     this.mapSize = size;
@@ -24,7 +24,7 @@ const DuckTravelModel = function(size = 7){
     }
 
     this.getDuckMap = function(){
-        
+
     }
 
     this.getDuckLocation = function(){
@@ -32,6 +32,10 @@ const DuckTravelModel = function(size = 7){
     }
     
     this.getCurLocation = function(){
+
+    }
+
+    this.getCurrentTime = function(){
 
     }
 
@@ -51,3 +55,4 @@ const DuckTravelModel = function(size = 7){
 
     }
 }
+
